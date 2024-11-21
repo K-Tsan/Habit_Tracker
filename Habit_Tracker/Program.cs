835598a557ca080dd29b9594cbe7d4404fe8c8f4
Kevin T <124527625+K-Tsan@users.noreply.github.com>
2024-11-21 15:46:23 +0000

    Create initial database

--- Habit_Tracker/Program.cs
+++ Habit_Tracker/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Habit_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=habitTracker.db";
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCommand = connection.CreateCommand();
+
+                tableCommand.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS drinking_water (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                tableCommand.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+    }
+}
+
